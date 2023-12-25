d5bb5f5b891975b95a33384c78c936b0bcc43503
sykwer <sykwer@gmail.com>
2023-12-25 10:53:50 +0900

    infinite sub queue size

--- ros2_thread_configurator/src/thread_configurator_node.cpp
+++ ros2_thread_configurator/src/thread_configurator_node.cpp
@@ -33,7 +33,7 @@ ThreadConfiguratorNode::ThreadConfiguratorNode(const YAML::Node &yaml) : Node("t
   }
 
   subscription_ = this->create_subscription<thread_config_msgs::msg::CallbackGroupInfo>(
-    "/ros2_thread_configurator/callback_group_info", 100, std::bind(&ThreadConfiguratorNode::topic_callback, this, std::placeholders::_1));
+    "/ros2_thread_configurator/callback_group_info", 0 /* infinite queue size*/, std::bind(&ThreadConfiguratorNode::topic_callback, this, std::placeholders::_1));
 }
 
 bool ThreadConfiguratorNode::all_applied() {
