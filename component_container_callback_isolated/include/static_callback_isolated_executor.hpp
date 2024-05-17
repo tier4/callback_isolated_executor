038dd021230500bfced6acf64f69acfa00e3c1d7
sykwer <sykwer@gmail.com>
2024-05-17 12:01:57 +0900

    Revert "add cmake function"
    This reverts commit 512fd65bff0b61204e577240cbe56223364e85ce.

--- component_container_callback_isolated/include/static_callback_isolated_executor.hpp
+++ component_container_callback_isolated/include/static_callback_isolated_executor.hpp
@@ -4,9 +4,8 @@
 class StaticCallbackIsolatedExecutor {
 public:
   void add_node(const rclcpp::Node::SharedPtr &node);
-  void add_node(const rclcpp::node_interfaces::NodeBaseInterface::SharedPtr &node);
   void spin();
 private:
-  //rclcpp::Node::SharedPtr node_;
-  rclcpp::node_interfaces::NodeBaseInterface::SharedPtr node_;
+  rclcpp::Node::SharedPtr node_;
 };
+
