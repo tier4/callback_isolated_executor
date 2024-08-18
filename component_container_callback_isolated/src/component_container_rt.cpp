816382ed4cb30fcff9f39805359cb126ae774718
Chih-Hung Tseng <chihungtzeng@gmail.com>
2024-08-18 09:21:44 +0800

    Add missing namespace brace (#6)
    * Add missing namespace brace
    
    Add missing namespace brace and return statement to make the build process succesful.
    
    * Explicitly shut down the node
    
    ---------
    
    Co-authored-by: chtseng <chtseng@itri.org.tw>

--- component_container_callback_isolated/src/component_container_rt.cpp
+++ component_container_callback_isolated/src/component_container_rt.cpp
@@ -18,7 +18,7 @@ public:
   template<typename... Args>
   ComponentManagerRt(Args&&... args) : rclcpp_components::ComponentManager(std::forward<Args>(args)...) {
     client_publisher_ = ros2_thread_configurator::create_client_publisher();
-    
+
     auto tid = syscall(SYS_gettid);
     for (int i = 0; i < 3; i++) {
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
@@ -33,6 +33,7 @@ private:
   void passRt();
   rclcpp::Publisher<thread_config_msgs::msg::CallbackGroupInfo>::SharedPtr client_publisher_;
 };
+};  // namespace rclcpp_components
 
 int main(int argc, char * argv[])
 {
@@ -42,4 +43,6 @@ int main(int argc, char * argv[])
   auto node = std::make_shared<rclcpp_components::ComponentManagerRt>();
   exec->add_node(node);
   exec->spin();
+  rclcpp::shutdown();
+  return 0;
 }
