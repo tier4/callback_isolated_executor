74b06df42a0b6982c035f60c28276eaf6fd19cf9
atsushi421 <atsushi.yano.2@tier4.jp>
2025-10-23 16:46:09 +0900

    fix

--- cie_thread_configurator/src/util.cpp
+++ cie_thread_configurator/src/util.cpp
@@ -96,7 +96,8 @@ std::map<std::string, std::string> get_hardware_info() {
   // Execute lscpu command and capture output
   std::array<char, 128> buffer;
   std::string result;
-  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("lscpu", "r"), pclose);
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen("/usr/bin/lscpu", "r"),
+                                                pclose);
 
   if (!pipe) {
     return hw_info;
