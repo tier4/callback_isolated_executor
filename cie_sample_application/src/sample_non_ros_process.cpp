95d6cbbe6828de11a9774d848c32ee9c18927ab3
atsushi421 <atsushi.yano.2@tier4.jp>
2025-12-24 13:08:49 +0900

    test: add sample_non_ros_process

---
+++ cie_sample_application/src/sample_non_ros_process.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "cie_thread_configurator/cie_thread_configurator.hpp"
+
+void worker_function() { std::cout << "Worker thread running" << std::endl; }
+
+int main(int /*argc*/, char ** /*argv*/) {
+  auto thread = cie_thread_configurator::spawn_cie_thread("standalone_worker",
+                                                          worker_function);
+  thread.join();
+  return 0;
+}
